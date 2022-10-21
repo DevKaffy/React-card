e7d297e4017caeae92acd5cf5d303f07ad098e52
Kafilat <kafilatadewumi61@gmail.com>
2022-10-21 10:55:00 +0100

    chore: clean up

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,21 @@
-// import React from 'react'
-import Card from './Component/Card'
-import './App.css'
-import {details} from '../data'
+import Card from "./Components/Card";
+import { details } from "./data";
 
+import "./App.css";
 
-  function App(){
-    return (
-      <div>
-    <div className='persons'>
-    <h1 className='react'>React-Card Project</h1>
-      {details.map((detail) => (
-        <Card {...detail} />
-      )
-      )}
-  </div>
-  <p className='footer'>React.js Examples Project React-card</p>
+function App() {
+
+  return (
+    <div>
+      <div className="persons">
+        <h1 className="react">React-Card Project</h1>
+        {details.map((detail) => (
+          <Card {...detail} />
+        ))}
       </div>
- 
-);
-      }
+      <p className="footer">React.js Examples Project React-card</p>
+    </div>
+  );
+}
 
-  export default App;
+export default App;
